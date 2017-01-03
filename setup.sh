6719404095bb5d8e378f71e3e3d7cb79ca1253f1
flopp <fpigorsch@gmail.com>
2017-01-03 20:05:48 +0100

    optimize setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ set -e
 function clone() {
     local URL="$1"
     local DIR="$2"
-
+    
     if [ ! -d "$DIR/.git" ] ; then
+        echo "Cloning $URL"
         git clone "$URL" "$DIR"
     else
+        echo "Updating from $URL"
         (cd "$DIR" ; git pull)
     fi
 }
@@ -35,7 +37,21 @@ fi
 CSS="src/css/main.css src/css/side-menu.css"
 
 B=".build"
+echo "creating $B"
 mkdir -p $B
+rm -f $B/*
+
+echo "copying html files"
 cp src/html/index.html $B
+if [ -f "google*.html" ] ; then
+    cp "google*.html" $B
+fi
+
+echo "copying js files"
 cat $JS > $B/scripts.js
+
+echo "copying css files"
 cat $CSS > $B/styles.css
+
+echo
+echo "You may now point your browser to 'file://$(pwd)/$B/index.html', or copy the contents of '$B' to a sever of your choice."
