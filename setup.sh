e5976ef678c01418060702a1e99a20e03b8e3686
flopp <fpigorsch@gmail.com>
2017-01-04 11:21:58 +0100

    force browser cache reload when css or js files are changed

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 set -e
+shopt -s dotglob
 
 function clone() {
     local URL="$1"
     local DIR="$2"
-    
+
     if [ ! -d "$DIR/.git" ] ; then
-        echo "Cloning $URL"
+        echo "-- cloning $URL"
         git clone "$URL" "$DIR"
     else
-        echo "Updating from $URL"
+        echo "-- updating from $URL"
         (cd "$DIR" ; git pull)
     fi
 }
@@ -28,28 +29,36 @@ clone "http://git.code.sf.net/p/geographiclib/code" ".local/geographiclib"
     .local/geographiclib/js/src/PolygonArea.js \
     > .local/geographiclib/js/geographicslib.js
 
+B=".build"
+echo "-- creating $B"
+mkdir -p $B
+rm -f $B/*
 
+echo "-- copying html files"
+cp src/html/index.html $B
+
+if [ -d private ] ; then
+    echo "-- copying private files"
+    cp private/* $B
+fi
+
+echo "-- copying js files"
 JS=".local/geographiclib/js/geographicslib.js src/js/utils.js src/js/latlng.js src/js/map.js src/js/app.js src/js/main.js src/js/side-menu.js"
 if [ -f analytics.js ] ; then
     JS="$JS analytics.js"
 fi
+cat $JS > $B/_scripts.js
+SCRIPTSJS=$(sha1sum $B/_scripts.js | cut -c-40).js
+mv $B/_scripts.js $B/$SCRIPTSJS
 
+echo "-- copying css files"
 CSS="src/css/main.css src/css/side-menu.css"
+cat $CSS > $B/_styles.css
+STYLESCSS=$(sha1sum $B/_styles.css | cut -c-40).css
+mv $B/_styles.css $B/$STYLESCSS
 
-B=".build"
-echo "creating $B"
-mkdir -p $B
-rm -f $B/*
-
-echo "copying html files"
-cp src/html/index.html $B
-cp -f google*.html $B
-
-echo "copying js files"
-cat $JS > $B/scripts.js
-
-echo "copying css files"
-cat $CSS > $B/styles.css
+echo "-- replacing template strings"
+sed -i -e "s/SCRIPTSJS/$SCRIPTSJS/g" -e "s/STYLESCSS/$STYLESCSS/g" $B/index.html
 
 echo
 echo "You may now point your browser to 'file://$(pwd)/$B/index.html', or copy the contents of '$B' to a sever of your choice."
